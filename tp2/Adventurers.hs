43826cf2d1773ef00aaba53dbee1561584130384
Paulo Ricardo Pereira <pg47554@alunos.uminho.pt>
2022-06-15 19:00:16 +0100

    diretorias

--- tp2/Adventurers.hs
+++ tp2/Adventurers.hs
@@ -99,44 +99,6 @@ combinationsUpTo2 = conc . (split f g) where
       remove x [] = []
       remove x (h:t) = if x==h then t else remove x t
 
-
-
---------------------------------------------------------------------------
--- Initial version - not very efficient because we are always comparing
--- the adventurer state with the lantern state
-allValidPlays2 :: State -> ListDur State
-allValidPlays2 st = rmEmptyMoves $ manyChoice $ 
-                   [advGoesAlone st P1] ++
-                   [advGoesAlone st P2] ++
-                   [advGoesAlone st P5] ++
-                   [advGoesAlone st P10] ++
-                   [advsGoTogether st (P1, P2)] ++
-                   [advsGoTogether st (P1, P5)] ++
-                   [advsGoTogether st (P1, P10)] ++
-                   [advsGoTogether st (P2, P5)] ++
-                   [advsGoTogether st (P2, P10)] ++
-                   [advsGoTogether st (P5, P10)]
-
--- One adventurer crosses
--- if adventurers cannot grab the lantern, the state remains the same
-advGoesAlone :: State -> Adventurer -> ListDur State
-advGoesAlone st adv =
-      if st (Left adv) == st lantern
-      then LD $ [Duration (getTimeAdv adv, mChangeState [Left adv, lantern] st)]
-      else return st
-
--- Two adventurers cross
--- if adventurers cannot grab the lantern, the state remains the same
-advsGoTogether :: State -> (Adventurer, Adventurer) -> ListDur State
-advsGoTogether st (adv1, adv2) =
-      if st (Left adv1) == st (Left adv2) && st (Left adv1) == st lantern
-      then LD $
-        [Duration (max (getTimeAdv adv1) (getTimeAdv adv2),
-         mChangeState [Left adv1, Left adv2, lantern] st)]
-      else return st
---------------------------------------------------------------------------
-
-
 {-- For a given number n and initial state, the function calculates
 all possible n-sequences of moves that the adventures can make --}
 -- To implement 
--- tp2/lixo.hs
+++ tp2/lixo.hs
@@ -3,3 +3,38 @@ teste2 f = let f' = teste f in kleisli f'
 
 kleisli :: Monad m => (a -> m b) -> m a -> m b
 kleisli = flip (>>=)
+
+--------------------------------------------------------------------------
+-- Initial version - not very efficient because we are always comparing
+-- the adventurer state with the lantern state
+allValidPlays2 :: State -> ListDur State
+allValidPlays2 st = rmEmptyMoves $ manyChoice $ 
+                   [advGoesAlone st P1] ++
+                   [advGoesAlone st P2] ++
+                   [advGoesAlone st P5] ++
+                   [advGoesAlone st P10] ++
+                   [advsGoTogether st (P1, P2)] ++
+                   [advsGoTogether st (P1, P5)] ++
+                   [advsGoTogether st (P1, P10)] ++
+                   [advsGoTogether st (P2, P5)] ++
+                   [advsGoTogether st (P2, P10)] ++
+                   [advsGoTogether st (P5, P10)]
+
+-- One adventurer crosses
+-- if adventurers cannot grab the lantern, the state remains the same
+advGoesAlone :: State -> Adventurer -> ListDur State
+advGoesAlone st adv =
+      if st (Left adv) == st lantern
+      then LD $ [Duration (getTimeAdv adv, mChangeState [Left adv, lantern] st)]
+      else return st
+
+-- Two adventurers cross
+-- if adventurers cannot grab the lantern, the state remains the same
+advsGoTogether :: State -> (Adventurer, Adventurer) -> ListDur State
+advsGoTogether st (adv1, adv2) =
+      if st (Left adv1) == st (Left adv2) && st (Left adv1) == st lantern
+      then LD $
+        [Duration (max (getTimeAdv adv1) (getTimeAdv adv2),
+         mChangeState [Left adv1, Left adv2, lantern] st)]
+      else return st
+--------------------------------------------------------------------------
